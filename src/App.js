4a92effe7a3804a55a1f39089acee923330d7ee5
Carlos Eduardo <cadu4duarte@gmail.com>
2022-01-06 21:03:30 -0300

    add style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
 import React, {Component} from "react";
-
+import "./style.css"
 class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        numero: 0,
+        botao: "Iniciar"
+      };
+
+      this.timer = null;
+
+      this.iniciar = this.iniciar.bind(this);
+      this.zerar = this.zerar.bind(this);
+    
+  }
+
+  iniciar() {
+    let state = this.state;
+
+    if(this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+      state.botao = "Iniciar";
+    } else {
+      this.timer = setInterval(() => {
+        let state = this.state;
+        state.numero += 0.1;
+        this.setState(state);
+      },100);
+      state.botao = "Pausar";
+    }
+
+    this.setState(state);
+    
+  }
+
+  zerar() {
+    if(this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+
+    let state = this.state;
+    state.numero = 0;
+    state.botao = "Iniciar";
+    this.setState(state);
+  }
+
   render() {
     return(
-      <div>
-        123
+      <div className="container">
+        <img src={require("./assets/cronometro.png")} className="img"/>
+        <a className="timer">{this.state.numero.toFixed(1)}</a>
+        <div className="areaBtn">
+          <a className="botao" onClick={this.iniciar}>{this.state.botao}</a>
+          <a className="botao" onClick={this.zerar}>Zerar</a>
+        </div>
       </div>
     );
   }
